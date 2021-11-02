db6deeaeeb17875d5b72b5a9a17b2e84e8631e1a
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2021-11-02 16:57:14 -0400

    🔧 Packaging files

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="bdqc_taxa",
+    version="0.0.1",
+    author="Vincent Beauregard",
+    author_email="vincent.beauregard@usherbrooke.ca",
+    description="`BIOQC-taxa` is a python package that interface with *Biodiversité Québec*'s database to query reference taxa sources, parse their return and generate records.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ReseauBiodiversiteQuebec/bdqc-taxa",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "bdqc_taxa"},
+    packages=setuptools.find_packages(where="bdqc_taxa"),
+    python_requires=">=3.6",
+)
