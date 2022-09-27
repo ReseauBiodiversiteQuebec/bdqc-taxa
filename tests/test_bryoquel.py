3a2f070629c437f668fc0bbebcd0ca1db4816292
Vincent Beauregard <vincent.beauregard@usherbrooke.ca>
2022-09-27 12:15:58 -0400

    ✨ Fit genus or family

--- tests/test_bryoquel.py
+++ tests/test_bryoquel.py
@@ -8,8 +8,8 @@ class TestBryoquel(TestCase):
         # Test a species that is in the database
         result = match_species(species)
         self.assertEqual(result['id'], 269)
-        self.assertEqual(result['family_scientific_name'], 'Aulacomniaceae')
-        self.assertEqual(result['species_scientific_name'], 'Aulacomnium palustre')
+        self.assertEqual(result['family'], 'Aulacomniaceae')
+        self.assertEqual(result['scientific_name'], 'Aulacomnium palustre')
         self.assertEqual(result['species_canonical_name'], 'Aulacomnium palustre (Hedw.) Schwägr.')
         self.assertEqual(result['vernacular_name_fr'], 'aulacomnie des marais')
         self.assertEqual(result['vernacular_name_en'], 'ribbed bog moss')
@@ -20,9 +20,19 @@ class TestBryoquel(TestCase):
         # Test a species that is in the database
         result = match_species(species)
         self.assertEqual(result['id'], 269)
-        self.assertEqual(result['family_scientific_name'], 'Aulacomniaceae')
-        self.assertEqual(result['species_scientific_name'], 'Aulacomnium palustre')
+        self.assertEqual(result['family'], 'Aulacomniaceae')
+        self.assertEqual(result['scientific_name'], 'Aulacomnium palustre')
         self.assertEqual(result['vernacular_name_fr'], 'aulacomnie des marais')
         self.assertEqual(result['vernacular_name_en'], 'ribbed bog moss')
         self.assertEqual(result['clade'], 'Musci')
         self.assertEqual(result['authorship'], '(Hedw.) Schwägr.')
+
+    def test_match_family(self, family='Aulacomniaceae'):
+        # Test a species that is in the database
+        result = match_species(family)
+        self.assertEqual(result['family'], 'Aulacomniaceae')
+        self.assertEqual(result['scientific_name'], 'Aulacomniaceae')
+        self.assertEqual(result['vernacular_name_fr'], None)
+        self.assertEqual(result['vernacular_name_en'], None)
+        self.assertEqual(result['clade'], 'Musci')
+        self.assertEqual(result['authorship'], None)
